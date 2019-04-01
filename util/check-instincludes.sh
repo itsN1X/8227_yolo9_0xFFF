89b36b2f30ae5130975021ea549fbf95c0aec99a
N1X <k9nikhil@gmail.com>
2019-04-01 15:57:42 +0200

    🈀
    https://twitter.com/desantis/status/1112682303485083648?s=20

---
+++ util/check-instincludes.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+#
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+#
+# Check the installed bind9 header files to make sure that no header
+# depends on another header having been included first, and that
+# they all compile as C++.
+#
+
+case $# in
+  1) ;;
+  *) echo "usage: sh util/check-instincludes.sh <prefix>" >&2;
+     exit 1;
+     ;;
+esac
+
+prefix=$1
+
+test -f ./configure.in || {
+    echo "$0: run from top of bind9 source tree" >&2;
+    exit 1;
+}
+
+tmp=/tmp/thdr$$.tmp
+
+status=0
+
+echo "Checking header independence and C++ compatibility..."
+
+# Make a list of header files.
+(cd $prefix/include; find . -name '*.h' -print | sed 's!^./!!') > $tmp
+
+# Check each header.
+while read h
+do
+    echo " - <$h>"
+
+    # Build a test program.
+    cat <<EOF >test.cc
+#include <$h>
+EOF
+
+    # Compile the test program.
+    if
+       gcc  -W -Wall -Wmissing-prototypes -Wcast-qual -Wwrite-strings \
+           -I/usr/pkg/pthreads/include -I$prefix/include -c test.cc 2>&1
+    then
+       :
+    else
+       status=1
+    fi
+done <$tmp
+
+rm -f test.cc test.o $tmp
+
+exit $status
---
+++ util/memleak.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+#
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+# Massage the output from ISC_MEM_DEBUG to extract mem_get() calls
+# with no corresponding mem_put().
+
+$mem_stats = '';
+
+while (<>) {
+    $gets{$1.$2} = $_ if (/add (?:0x)?([0-9a-f]+) size (?:0x)?([0-9]+) file/);
+    delete $gets{$1.$2} if /del (?:0x)?([0-9a-f]+) size (?:0x)?([0-9]+) file/;
+    $mem_stats .= $_ if /\d+ gets, +(\d+) rem/ && $1 > 0;
+}
+print join('', values %gets);
+print $mem_stats;
+
+exit(0);
